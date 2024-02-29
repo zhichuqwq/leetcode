c4b1c31bddfca6611c6bd07431bcf56728c4e813
zhichuqwq <124870542+zhichuqwq@users.noreply.github.com>
2024-02-29 22:05:20 +0800

    Create Solution.java

---
+++ 2865.美丽塔 I/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long maximumSumOfHeights(List<Integer> maxHeights) {
+        int n = maxHeights.size();
+        long res = 0;
+        for (int i = 0; i < n; i++) {
+            int pre = maxHeights.get(i);
+            long sum = pre;
+            for (int j = i - 1; j >= 0; j--) {
+                pre = Math.min(pre, maxHeights.get(j));
+                sum += pre;
+            }
+            int suf = maxHeights.get(i);
+            for (int j = i + 1; j < n; j++) {
+                suf = Math.min(suf, maxHeights.get(j));
+                sum += suf;
+            }
+            res = Math.max(res, sum);
+        }
+        return res;
+    }
+}
