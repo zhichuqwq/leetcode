8a09e883035993e15517cba42068b7270d618802
zhichuqwq <124870542+zhichuqwq@users.noreply.github.com>
2024-02-29 21:59:37 +0800

    Create Solution.java

---
+++ 2583.二叉树中的第K大层和/Solution.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public long kthLargestLevelSum(TreeNode root, int k) {
+        Queue<TreeNode> queue = new ArrayDeque<TreeNode>();
+        queue.offer(root);
+        List<Long> levelSumArray = new ArrayList<Long>();
+        while (!queue.isEmpty()) {
+            List<TreeNode> levelNodes = new ArrayList<TreeNode>(queue);
+            long levelSum = 0;
+            queue.clear();
+            for (TreeNode node : levelNodes) {
+                levelSum += node.val;
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            levelSumArray.add(levelSum);
+        }
+        if (levelSumArray.size() < k) {
+            return -1;
+        }
+        Collections.sort(levelSumArray);
+        return levelSumArray.get(levelSumArray.size() - k);
+    }
+}
