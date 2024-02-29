469cbf8f495e63feab2e372f96ef7839c67ed88f
zhichuqwq <124870542+zhichuqwq@users.noreply.github.com>
2024-02-29 22:00:46 +0800

    Create Solution.java

---
+++ 2673.使二叉树所有路径值相等的最小代价/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minIncrements(int n, int[] cost) {
+        int ans = 0;
+        for (int i = n - 2; i > 0; i -= 2) {
+            ans += Math.abs(cost[i] - cost[i + 1]);
+            // 叶节点 i 和 i+1 的双亲节点下标为 i/2（整数除法）
+            cost[i / 2] += Math.max(cost[i], cost[i + 1]);
+        }
+        return ans;
+    }
+}
